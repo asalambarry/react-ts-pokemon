5fb37a457931bb33e9c083bfc02e8399dbcff387
Barry <a.salambarry@gmail.com>
2024-02-20 23:27:45 +0100

    app pokemon-react-ts

---
+++ src/Age.tsx
@@ -0,0 +1,19 @@
+// import React , {useState}from 'react'
+// type AgeProperties = {
+//     age: number
+// }
+// const Age = (properties : AgeProperties) => {
+//     // const {age} = AgeProperties
+//     const [age, setAge] = useState<AgeProperties>()
+//   return (
+//     <div>
+//         {age > 18 ? (
+//             <p>Vous etes majeur, donc vous pouvez voir ce contenu</p>
+//         ): (
+//             <p></p>
+//         )}
+//     </div>
+//   )
+// }
+
+// export default Age
